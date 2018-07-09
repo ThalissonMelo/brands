684d2e98193bb29482b1ee4ad7a805eed45dd17f
ThalissonMelo <tallisonmelo@hotmail.com>
2018-07-09 16:23:50 -0300

    Adding project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :products
+  resources :brands
+  post "users/:id/follow", to: "users#add_follow_brands"
+  get "users/:id/brands", to: "users#show_follow_brands"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
